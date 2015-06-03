ad808025cb5bd058c95ee92099f0cf685b5e561d
marcosptf <marcosptf@yahoo.com.br>
2015-06-03 18:05:45 -0300

    commit junit tests

---
+++ src/junittests/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package junittests;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+/**
+ *
+ * @author marcos
+ */
+public class main {
+    public static void main(String[] args){
+        Result result = JUnitCore.runClasses(TestJunit1.class);
+        for(Failure failure : result.getFailures()){
+            System.out.println(failure.toString());
+        }
+        System.out.println(result.wasSuccessful());
+    }
+}
