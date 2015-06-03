0558bbf929057d32d1befd5fdb95e144ba6e6d1a
marcosptf <marcosptf@yahoo.com.br>
2015-06-03 19:49:29 -0300

    commit junit tests

---
+++ src/junittests/TestJunit3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package junittests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+/**
+ *
+ * @author marcos
+ */
+public class TestJunit3 {
+    @Test
+    public void testAdd(){
+        int num = 5;
+        String str = "abc";
+        
+        assertEquals("aBc",str);
+        assertFalse(num > 4);
+        assertNotNull(str);
+    }
+}
