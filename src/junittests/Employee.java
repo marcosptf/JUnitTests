5070fbc630c261bf68af528d375b877328313350
p0w34007 <p0w34007@yahoo.com>
2015-06-04 13:34:12 -0300

    Create Employee.java

---
+++ src/junittests/Employee.java
@@ -0,0 +1,52 @@
+public class Employee {
+/*from   ww w  . j  a  va 2s. c o m*/
+   private String name;
+   private double salary;
+   private int age;
+   
+   /**
+   * @return the name
+   */
+   public String getName() {
+      return name;
+   }
+   /**
+   * @param name the name to set
+   */
+   public void setName(String name) {
+      this.name = name;
+   }
+   /**
+   * @return the monthlySalary
+   */
+   public double getSalary() {
+      return salary;
+   }
+   /**
+   * @param monthlySalary the monthlySalary to set
+   */
+   public void setSalary(double s) {
+      this.salary = s;
+   }
+   /**
+   * @return the age
+   */
+   public int getAge() {
+      return age;
+   }
+   /**
+   * @param age the age to set
+   */
+   public void setAge(int age) {
+   this.age = age;
+   }
+}
+
+public class EmpBusinessLogic {
+   // Calculate the yearly salary of employee
+   public double calculateYearlySalary(Employee employee){
+      double yearlySalary=0;
+      yearlySalary = employee.getSalary() * 12;
+      return yearlySalary;
+   }
+}
